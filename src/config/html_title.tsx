4427eb06e9b55ce24bf8f49039d1cc1e299e2433
Simon Rappenecker <simi2657@gmail.com>
2025-10-19 20:28:45 +0200

    fixed assets for build

--- src/config/html_title.tsx
+++ src/config/html_title.tsx
@@ -2,6 +2,7 @@
 export const config = {
   // Website title, static accross all pages
   title: 'Your Name',
-  // Website icon, static accross all pages, use alternatively a local file, e. g. src/assets/myicon.svg
+  // Website icon, static accross all pages, use alternatively a local file, e. g. /assets/myicon.svg
+  // Here note: Put your assets in the folder public/assets NOT into assets/ !
   icon: 'https://avatars.githubusercontent.com/u/16435889?v=4',
 };
